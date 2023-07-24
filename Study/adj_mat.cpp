62c61b70df55058bc5ae43668256d6a496bd72ed
soz1p <jeonsojin1363@hansung.ac.kr>
2023-07-24 20:27:22 +0900

    인접배열, 인접리스트 코드 리펙토링

--- Study/adj_mat.cpp
+++ Study/adj_mat.cpp
@@ -17,8 +17,8 @@ void init(GraphType* g) {
     }
 }
 
-//item 받는 이유는 뭘까?
-void insert_vertex(GraphType* g) {
+//v 받는 이유는 뭘까? -> 이유 없음
+void insert_vertex(GraphType* g, int v) {
     if(((g->n) + 1) > MAX_VERTICS) {
         fprintf(stderr, "그래프 : 정점의 개수 초과");
         return;
@@ -26,12 +26,42 @@ void insert_vertex(GraphType* g) {
     g->n++;
 }
 
+void insert_edge(GraphType* g, int start, int end) {
+    if(start >= g->n || end >= g->n) {
+        fprintf(stderr, "그래프 : 정점 번호 오류"); //버퍼 바로 출력
+        return;
+    }
+    g->adj_mat[start][end] = 1;
+    g->adj_mat[end][start] = 1;
+
+}
+
+void print_adj_mat(GraphType* g) {
+    for(int i = 0; i < g->n; i++) {
+        for(int j = 0; j < g->n; j++) {
+            printf("%2d ", g->adj_mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     GraphType *g;
-    g = (GraphType *)malloc(sizeof(GraphType));
+    g = (GraphType *)malloc(sizeof(GraphType);
     init(g);
 
-    for(int i=0; i<51; i++) {
-        insert_vertex(g);
+    for(int i=0; i<4; i++) {
+        insert_vertex(g, i);
     }
+
+    //무방향 그래프를 가정하고..?
+    insert_edge(g, 0, 1);
+    insert_edge(g, 0, 2);
+    insert_edge(g, 0, 3);
+    insert_edge(g, 1, 2);
+    insert_edge(g, 2, 3);
+
+    print_adj_mat(g);
+
+    free(g);
 }
